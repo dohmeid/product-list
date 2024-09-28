0ba1089466b1215e1f7525bd0ed1e1836c1a78d9
dohmeid <doha.hmeid@gmail.com>
2024-09-29 01:30:43 +0300

    Create basic layout for Filter and Header components

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from './Filter.module.css';
+
+const Filter = () => {
+
+    return (
+        <div>
+            <p>this is the products filter</p>
+        </div>
+    );
+}
+
+export default Filter;
